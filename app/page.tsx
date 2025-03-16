e3dce5440fe4f8c873d4eb63b6eddcd60678a920
Mustafa Hasanain <m7asanain@gmail.com>
2025-03-16 09:09:36 +0300

    Bento Grid (issue: small screen layout)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/floatingNavbar";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome />}]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
